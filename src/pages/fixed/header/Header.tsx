ccb58f2c206866fd2c990ff0a49483560befd346
bryanStv <bryans.oyola@gmail.com>
2025-04-16 12:55:21 +0200

    Header realizado con barra de búsqueda

--- src/pages/fixed/header/Header.tsx
+++ src/pages/fixed/header/Header.tsx
@@ -1,12 +1,28 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <div className={styles.links}>
+          <Link to="/">Inicio</Link>
+          <Link to="/about">About</Link>
+        </div>
+        <input
+          type="text"
+          placeholder="Buscar pokémon..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className={styles.searchInput}
+        />
       </nav>
     </header>
   );
