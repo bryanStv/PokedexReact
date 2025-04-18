8ee91fa0429bf615d948ebba82523e1a827555fa
bryanStv <bryans.oyola@gmail.com>
2025-04-18 21:37:59 +0200

    First footer

---
+++ src/pages/fixed/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import style from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={style.footer}>
+      <h1>By bryanStv</h1>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../pages/fixed/header/Header";
+import Footer from "../pages/fixed/footer/Footer";
 
 const RootLayout = () => {
   return (
@@ -8,6 +9,7 @@ const RootLayout = () => {
       <main style={{ padding: "1rem" }}>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };
